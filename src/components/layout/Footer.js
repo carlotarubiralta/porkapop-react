b7f5af7cec336a600be60f1ede689af50e832051
Carlota Rubiralta <carlalrmkt@gmail.com>
2024-07-13 14:04:56 +0200

    maquetacion header

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
-    <footer className="text-center mt-5">
-      <p>© 2023 Nodepop. Todos los derechos reservados.</p>
+    <footer className="footer">
+      <p>Desarrollado por Carlota Rubiralta</p>
     </footer>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import AuthButton from '../auth/AuthButton';
+import logo from '../../assets/images/logo.png'; // Asegúrate de tener un logo en esta ruta
+import './Header.css';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg" className="header">
-      <Navbar.Brand as={Link} to="/adverts">Nodepop</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/adverts">Anuncios</Nav.Link>
-        <Nav.Link as={Link} to="/adverts/new">Crear Anuncio</Nav.Link>
-      </Nav>
-      <Nav>
-        <AuthButton onLogout={() => {}} />
-      </Nav>
+    <Navbar expand="lg" className="header">
+      <Container>
+        <Navbar.Brand as={Link} to="/adverts">
+          <img
+            src={logo}
+            height="30"
+            className="d-inline-block align-top"
+            alt="Logo"
+          />
+          
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/adverts">Anuncios</Nav.Link>
+            <Nav.Link as={Link} to="/adverts/new">Crear Anuncio</Nav.Link>
+            <AuthButton onLogout={() => {}} />
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
